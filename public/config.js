26aaba67c4dfc810e4503352fcda18e72c51995a
Sumit Ghosh <317555249+sumitghosh1405@users.noreply.github.com>
2026-09-24 23:02:34 +0530

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 // Paste your Google Maps JavaScript API key between the quotes, then redeploy.
 // Restrict the key to your website address in Google Cloud so nobody else can use it.
-window.NC_CONFIG={mapsKey:''};
+window.NC_CONFIG={mapsKey:'AIzaSyBZIVPtQr3D91QbPPbmelgoUQGOAZsyw6g'};
