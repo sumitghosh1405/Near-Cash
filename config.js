680f59eaade6fe5eec99657ef1a74431137da103
Sumit Ghosh <317555249+sumitghosh1405@users.noreply.github.com>
2026-09-24 15:52:58 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Paste your Google Maps JavaScript API key between the quotes, then redeploy.
-// Restrict the key to your website address in Google Cloud so nobody else can use it.
-window.NC_CONFIG={mapsKey:''};
